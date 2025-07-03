642f27c4cca6c6a404a3b02916a3dda1b6a05ddd
Canh Ta <canhta.w@gmail.com>
2025-07-04 01:08:46 +0700

    test

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -33,6 +33,7 @@ export default tseslint.config(
       '@typescript-eslint/no-unsafe-call': 'warn',
       '@typescript-eslint/no-unsafe-member-access': 'warn',
       '@typescript-eslint/no-unsafe-return': 'warn',
+      '@typescript-eslint/require-await': 'warn',
     },
   },
 );
